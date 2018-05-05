f8b09d4789142e2082fc6d55c7014df6f4d3856d
pkt97 <37771197+pkt97@users.noreply.github.com>
2018-05-05 21:33:48 +0530

    Create BufferOverflow.java

---
+++ BufferOverflow.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class BufferOverflow
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    char a[]=new char[5];
+	    a[0]='h';
+	    a[1]='e';
+	    a[2]='l';
+	    a[3]='l';
+	    a[4]='o';
+	    a[5]='a';
+	    a[6]='l';
+	    a[7]='l';
+	    System.out.println("The array is"+a);
+		// your code goes here
+	}
+}
